1c8ee5ca7e63eb0ba656ea4138c8a480ac1eeb18
Tommaso Tedeschi <tedesk95@gmail.com>
2020-11-20 09:45:39 +0100

    Create deploy_monitoring.sh

---
+++ deploy_monitoring.sh
@@ -0,0 +1,5 @@
+kubectl apply -f manifests_no_configs/exporter.yaml
+kubectl create namespace monitoring
+kubectl create configmap prometheus-example-cm --from-file configs/prometheus.yml -n monitoring
+kubectl apply -f manifests_no_configs/prometheus.yamlkubectl create configmap prometheus-adapter-example-cm --from-file configs/prometheus_adapter.yml -n monitoring
+kubectl apply -f manifests_no_configs/prometheus_adapter.yaml -n monitoring
