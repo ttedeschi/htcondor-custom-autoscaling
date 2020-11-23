2d08f9e3d83509448df0f10338ddbc5a254bb3d1
root <root@vnode-1.localdomain>
2020-11-23 09:38:28 +0000

    modified:   configs/prometheus_adapter.yml 	modified:   delete_monitoring.sh 	modified:   manifests_no_configs/exporter.yaml 	new file:   manifests_no_configs/hpa.yaml

--- delete_monitoring.sh
+++ delete_monitoring.sh
@@ -1,5 +1,5 @@
 kubectl delete -f manifests_no_configs/exporter.yaml
 kubectl delete configmap prometheus-example-cm -n monitoring
 kubectl delete -f manifests_no_configs/prometheus.yaml
-kubectl delete configmap prometheus-adapter-example-cm -n monitoring
-kubectl delete -f manifests_no_configs/prometheus_adapter.yaml -n monitoring
+kubectl delete configmap prometheus-adapter-example-cm 
+kubectl delete -f manifests_no_configs/prometheus_adapter.yaml 
